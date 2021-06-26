d093cd7578548c10dab3e0dce0e53410d063758b
Waranthorn Chansawang <waranthorn_c@outlook.com>
2021-06-26 21:51:29 +0700

    update

--- app.py
+++ app.py
@@ -13,39 +13,32 @@ import urllib
 import nltk
 # nltk.download('punkt')
 
-#Loading Flask and assigning the model variable
 app = Flask(__name__)
 CORS(app)
 app=flask.Flask(__name__,template_folder='templates')
 
-app = Flask(__name__)
-CORS(app)
-app=flask.Flask(__name__,template_folder='templates')
-
-with open('model.pickle', 'rb') as handle:
+with open('ex_news-tra_thai-model.pickle', 'rb') as handle:
 	model = pickle.load(handle)
 
 @app.route('/')
 def main():
     return render_template('index.html')
 
-#Receiving the input url from the user and using Web Scrapping to extract the news content
-@app.route('/predict',methods=['GET','POST'])
-def predict():
-    url =request.get_data(as_text=True)[5:]
-    print(url)
-    url = urllib.parse.unquote(url)
-    print(url)
-    article = Article(str(url), language='zh')
-    print(article)
-    article.download()
-    article.parse() # crashed
-    article.nlp()
-    news = article.summary
-    #Passing the news article to the model and returing whether it is Fake or Real
+def fake_news_det(news):
     pred = model.predict([news])
-    return render_template('index.html', prediction_text='The news is "{}"'.format(pred[0]))
-    
+    return pred
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        message = request.form['message']
+        print(message)
+        pred = fake_news_det(message)
+        print(pred)
+        return render_template('index.html', prediction=pred)
+    else:
+        return render_template('index.html', prediction="Something went wrong")
+
 if __name__=="__main__":
-    port=int(os.environ.get('PORT',5000))
+    port=int(os.environ.get('PORT', 5000))
     app.run(port=port,debug=True,use_reloader=False)
