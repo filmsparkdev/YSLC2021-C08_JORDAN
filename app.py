ca146cedd4da9fe917baa7b750a7b2466a621e90
Waranthorn Chansawang <waranthorn_c@outlook.com>
2021-06-26 19:52:30 +0700

    delete venv

--- app.py
+++ app.py
@@ -18,6 +18,10 @@ app = Flask(__name__)
 CORS(app)
 app=flask.Flask(__name__,template_folder='templates')
 
+app = Flask(__name__)
+CORS(app)
+app=flask.Flask(__name__,template_folder='templates')
+
 with open('model.pickle', 'rb') as handle:
 	model = pickle.load(handle)
 
@@ -29,19 +33,19 @@ def main():
 @app.route('/predict',methods=['GET','POST'])
 def predict():
     url =request.get_data(as_text=True)[5:]
+    print(url)
     url = urllib.parse.unquote(url)
-    article = Article(str(url))
+    print(url)
+    article = Article(str(url), language='zh')
+    print(article)
     article.download()
-    article.parse()
+    article.parse() # crashed
     article.nlp()
     news = article.summary
-    print('url: ', url)
-    print('article: ', article)
     #Passing the news article to the model and returing whether it is Fake or Real
     pred = model.predict([news])
-    print('news: ', news)
     return render_template('index.html', prediction_text='The news is "{}"'.format(pred[0]))
     
 if __name__=="__main__":
     port=int(os.environ.get('PORT',5000))
-    app.run(port=port, debug=True, use_reloader=False)
+    app.run(port=port,debug=True,use_reloader=False)
